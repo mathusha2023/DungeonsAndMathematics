c18acba722eac45e128a76d6b10bdc52bbbf926d
Alexey <bazooka2008@inbox.ru>
2023-12-26 21:05:36 +0300

    commit

---
+++ buttons.py
@@ -0,0 +1,35 @@
+import pygame
+
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self, *groups, text="Button", x=0, y=0,
+                 font=None, f_size=24, f_color=(255, 255, 255), f_active_color=(255, 255, 0), press_event=None):
+        super().__init__(*groups)
+        font1 = pygame.font.Font(font, f_size)
+        font2 = pygame.font.Font(font, f_size + 6)
+        self.text1 = font1.render(text, True, f_color)
+        self.text2 = font2.render(text, True, f_active_color)
+        self.center = (x, y)
+        self.config_image(1)
+        self.event = press_event
+
+    def config_image(self, im):
+        if im == 1:
+            self.image = self.text1
+        else:
+            self.image = self.text2
+        self.rect = self.image.get_rect()
+        self.rect.center = self.center
+
+    def update(self, *args):
+        if args:
+            event = args[0]
+            if event.type == pygame.MOUSEMOTION:
+                if self.rect.collidepoint(*event.pos):
+                    self.config_image(2)
+                else:
+                    self.config_image(1)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1 and self.rect.collidepoint(*event.pos):
+                    if self.event is not None:
+                        self.event()
--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("Hello agomus!")
+import pygame
+import sys
+import buttons
+
+pygame.init()
+size = width, height = 600, 600
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Dungeons&Mathematics")
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+menu_buttons = pygame.sprite.Group()
+
+buttons.Button(menu_buttons, text="Играть!", x=width // 2, y=300, f_size=40,
+               press_event=lambda: print("PLAYING!"))
+buttons.Button(menu_buttons, text="Рекорды", x=width // 2, y=350, f_size=40,
+               press_event=lambda: print("RECORDS!"))
+buttons.Button(menu_buttons, text="Настройки", x=width // 2, y=400, f_size=40,
+               press_event=lambda: print("SETTINGS!"))
+buttons.Button(menu_buttons, text="Выход", x=width // 2, y=450, f_size=40,
+               press_event=terminate)
+
+fps = 60
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
+        menu_buttons.update(event)
+    screen.fill((0, 0, 0))
+    menu_buttons.draw(screen)
+    pygame.display.flip()
+    clock.tick(fps)
