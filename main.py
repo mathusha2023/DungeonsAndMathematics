5cdf95fb66d15e534df454d9b3d576329539a388
Alexey <bazooka2008@inbox.ru>
2023-12-26 21:51:19 +0300

    add consts file

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import pygame
+import consts
 import buttons
 import specfunctions
 
 pygame.init()
-size = width, height = 600, 600
-screen = pygame.display.set_mode(size)
+
+screen = pygame.display.set_mode(consts.SIZE)
 pygame.display.set_caption("Dungeons&Mathematics")
 
 menu_buttons = pygame.sprite.Group()
 
-buttons.Button(menu_buttons, text="Играть!", x=width // 2, y=300, f_size=40,
+buttons.Button(menu_buttons, text="Играть!", x=consts.WIDTH // 2, y=300, f_size=40,
                press_event=lambda: print("PLAYING!"))
-buttons.Button(menu_buttons, text="Рекорды", x=width // 2, y=350, f_size=40,
+buttons.Button(menu_buttons, text="Рекорды", x=consts.WIDTH // 2, y=350, f_size=40,
                press_event=lambda: print("RECORDS!"))
-buttons.Button(menu_buttons, text="Настройки", x=width // 2, y=400, f_size=40,
+buttons.Button(menu_buttons, text="Настройки", x=consts.WIDTH // 2, y=400, f_size=40,
                press_event=lambda: print("SETTINGS!"))
-buttons.Button(menu_buttons, text="Выход", x=width // 2, y=450, f_size=40,
+buttons.Button(menu_buttons, text="Выход", x=consts.WIDTH // 2, y=450, f_size=40,
                press_event=specfunctions.terminate)
 
-fps = 60
+
 clock = pygame.time.Clock()
 
 while True:
@@ -29,4 +30,4 @@ while True:
     screen.fill((0, 0, 0))
     menu_buttons.draw(screen)
     pygame.display.flip()
-    clock.tick(fps)
+    clock.tick(consts.FPS)
