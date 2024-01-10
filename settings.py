1aeb9675bfcc968d1223b55b05ccf58f6a7f4a67
Alexey <bazooka2008@inbox.ru>
2024-01-10 21:52:20 +0300

    продолжение настроек 4

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ class SliderText(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.slider = slider
-        self.font = pygame.font.Font(None, 34)
+        self.font = pygame.font.Font(None, 45)
         self.update()
 
     def update(self, *args):
@@ -31,6 +31,15 @@ class SliderText(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = self.x
         self.rect.y = self.y
+
+
+class Text(pygame.sprite.Sprite):
+    def __init__(self, y, text, *groups):
+        super().__init__(*groups)
+        self.image = pygame.font.Font(None, 45).render(text, True, (255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.x = consts.WIDTH // 2 - 320
+        self.rect.y = y
 
 
 def update_settings(music, sound, lang):
@@ -45,23 +54,27 @@ def settings_menu():
     all_sprites = pygame.sprite.Group()
     button = buttons.EscapeButton(all_sprites, text="Назад", x=consts.WIDTH // 2, y=650, f_size=45)
 
-    music_slider = Slider(consts.SCREEN, consts.WIDTH // 2, 100, 400, 20,
+    music_slider = Slider(consts.SCREEN, consts.WIDTH // 2 - 100, 100, 400, 20,
                           min=0, max=100, step=1, colour=(120, 120, 120),
                           handleColour=(200, 200, 200), initial=Settings.vol_music * 100)
 
-    sound_slider = Slider(consts.SCREEN, consts.WIDTH // 2, 250, 400, 20,
+    sound_slider = Slider(consts.SCREEN, consts.WIDTH // 2 - 100, 250, 400, 20,
                           min=0, max=100, step=1, colour=(120, 120, 120),
                           handleColour=(200, 200, 200), initial=Settings.vol_sound * 100)
 
-    SliderText(consts.WIDTH // 2 + 200, 140, music_slider, all_sprites)
-    SliderText(consts.WIDTH // 2 + 200, 290, sound_slider, all_sprites)
+    SliderText(consts.WIDTH // 2 + 100, 140, music_slider, all_sprites)
+    SliderText(consts.WIDTH // 2 + 100, 290, sound_slider, all_sprites)
 
-    dropdown = Dropdown(consts.SCREEN, consts.WIDTH // 2, 400, 400, 40,
+    dropdown = Dropdown(consts.SCREEN, consts.WIDTH // 2 - 100, 400, 400, 40,
                         name=Settings.langs[Settings.language],
                         choices=Settings.langs.values(),
                         values=[Settings.eng, Settings.rus],
-                        textColour=(255, 255, 255), fontSize=34, inactiveColour=(0, 0, 0), hoverColour=(120, 120, 120),
+                        textColour=(255, 255, 255), fontSize=38, inactiveColour=(0, 0, 0), hoverColour=(120, 120, 120),
                         pressedColour=(120, 120, 120))
+
+    Text(100, "Музыка:", all_sprites)
+    Text(250, "Звуки:", all_sprites)
+    Text(405, "Язык:", all_sprites)
 
     while True:
         events = pygame.event.get()
