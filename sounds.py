e6400081b1d4787221081a3989b58d072188c366
MrLeviReaper <kocheevvadya@yandex.ru>
2024-01-13 16:56:59 +0300

    suicide

--- sounds.py
+++ sounds.py
@@ -92,7 +92,7 @@ class BossPhrases:
 
     def load_phrases(self):
         for i in range(1, 8):
-            sound = pygame.mixer.Sound(f"data/audio/bossphrases/phrase{i}_rus.wav")
+            sound = pygame.mixer.Sound(f"data/audio/bossphrases/ru/phrase{i}_rus.wav")
             sound.set_volume(settings.vol_sound)
             self.p.append(sound)
 
